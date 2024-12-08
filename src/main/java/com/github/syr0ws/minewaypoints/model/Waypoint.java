7034b93dd402b0e7d4aafa602482f9a85a7fc08a
Syrows <clement.gho@gmail.com>
2024-12-08 16:51:58 +0100

    adding model

---
+++ src/main/java/com/github/syr0ws/minewaypoints/model/Waypoint.java
@@ -0,0 +1,75 @@
+package com.github.syr0ws.minewaypoints.model;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class Waypoint {
+
+    private final long id;
+    private final String world;
+    private final double x, y, z;
+
+    private String name;
+    private Material icon;
+
+    public Waypoint(long id, String world, double x, double y, double z, String name, Material icon) {
+        this.id = id;
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.setName(name);
+        this.setIcon(icon);
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getWorld() {
+        return this.world;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("name cannot be null or empty");
+        }
+
+        this.name = name;
+    }
+
+    public Material getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(Material icon) {
+
+        if(icon == null || icon.isAir()) {
+            throw new IllegalArgumentException("icon cannot be null or empty");
+        }
+
+        this.icon = icon;
+    }
+
+    public Location getLocation() {
+        return new Location(Bukkit.getWorld(this.world), this.x, this.y, this.z);
+    }
+}
