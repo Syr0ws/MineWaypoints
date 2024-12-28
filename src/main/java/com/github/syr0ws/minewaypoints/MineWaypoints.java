8a708a5e47a4022277e696b273f98f4985c71d41
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2024-12-28 18:31:39 +0100

    feat: adding database connection (#5)

--- src/main/java/com/github/syr0ws/minewaypoints/MineWaypoints.java
+++ src/main/java/com/github/syr0ws/minewaypoints/MineWaypoints.java
@@ -5,23 +5,40 @@ import com.github.syr0ws.craftventory.api.config.action.ClickActionLoaderFactory
 import com.github.syr0ws.craftventory.api.config.dao.InventoryConfigDAO;
 import com.github.syr0ws.craftventory.common.CraftVentoryLibrary;
 import com.github.syr0ws.minewaypoints.command.CommandWaypoints;
+import com.github.syr0ws.minewaypoints.database.DatabaseConnection;
+import com.github.syr0ws.minewaypoints.database.DatabaseConnectionConfig;
+import com.github.syr0ws.minewaypoints.database.DatabaseConnectionFactory;
+import com.github.syr0ws.minewaypoints.database.DatabaseConnectionLoader;
 import com.github.syr0ws.minewaypoints.menu.WaypointDeleteMenuDescriptor;
 import com.github.syr0ws.minewaypoints.menu.WaypointIconsMenuDescriptor;
 import com.github.syr0ws.minewaypoints.menu.WaypointsMenuDescriptor;
 import com.github.syr0ws.minewaypoints.menu.action.OpenDeleteWaypointMenuLoader;
 import com.github.syr0ws.minewaypoints.menu.action.OpenWaypointIconsMenuLoader;
 import com.github.syr0ws.minewaypoints.menu.action.UpdateWaypointIconLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class MineWaypoints extends JavaPlugin {
 
     private InventoryService inventoryService;
 
+    private DatabaseConnection connection;
+
     @Override
     public void onEnable() {
 
         this.loadConfiguration();
+
+        try {
+            this.loadDatabase();
+        } catch (Exception exception) {
+            this.getLogger().log(Level.SEVERE, "An error occurred while creating the database connection", exception);
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
 
         this.registerInventoryProviders();
         this.registerCommands();
@@ -29,6 +46,17 @@ public class MineWaypoints extends JavaPlugin {
 
     private void loadConfiguration() {
         super.saveDefaultConfig();
+    }
+
+    private void loadDatabase() throws Exception {
+
+        DatabaseConnectionLoader loader = new DatabaseConnectionLoader(this);
+        DatabaseConnectionConfig config = loader.loadConfig();
+
+        DatabaseConnectionFactory factory = new DatabaseConnectionFactory(this);
+
+        this.connection = factory.createDatabaseConnection(config.getDriver());
+        this.connection.openConnection(config);
     }
 
     private void registerCommands() {
---
+++ src/main/java/com/github/syr0ws/minewaypoints/database/DatabaseDriver.java
@@ -0,0 +1,25 @@
+package com.github.syr0ws.minewaypoints.database;
+
+public enum DatabaseDriver {
+
+    SQLITE("sqlite", "org.sqlite.JDBC"),
+    MYSQL("mysql", "com.mysql.cj.jdbc.Driver"),
+    MARIADB("mariadb", "org.mariadb.jdbc.Driver"),
+    POSTGRESQL("postgresql", "org.postgresql.Driver");
+
+    private final String driverName;
+    private final String driverClass;
+
+    DatabaseDriver(String driverName, String driverClass) {
+        this.driverName = driverName;
+        this.driverClass = driverClass;
+    }
+
+    public String getDriverName() {
+        return this.driverName;
+    }
+
+    public String getDriverClass() {
+        return this.driverClass;
+    }
+}
