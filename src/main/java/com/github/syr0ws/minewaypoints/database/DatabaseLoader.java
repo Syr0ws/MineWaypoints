753a0d31a20ba4ce3578e353e431f66ffec58222
Syrows <clement.gho@gmail.com>
2024-12-28 16:33:49 +0100

    adding database loader

---
+++ src/main/java/com/github/syr0ws/minewaypoints/database/DatabaseLoader.java
@@ -0,0 +1,65 @@
+package com.github.syr0ws.minewaypoints.database;
+
+import com.github.syr0ws.minewaypoints.exception.ConfigurationException;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Arrays;
+
+public class DatabaseLoader {
+
+    private final Plugin plugin;
+
+    public DatabaseLoader(Plugin plugin) {
+
+        if(plugin == null) {
+            throw new IllegalArgumentException("plugin cannot be null");
+        }
+
+        this.plugin = plugin;
+    }
+
+    public Database loadConnection() throws ConfigurationException {
+
+        FileConfiguration config = this.plugin.getConfig();
+        ConfigurationSection section = config.getConfigurationSection("database");
+
+        String host = section.getString("host", "localhost");
+        String port = section.getString("port", "3306");
+        String database = section.getString("database", "minewaypoints");
+        String username = section.getString("username", "root");
+        String password = section.getString("password", "");
+
+        DatabaseDriver driver = this.loadDriver(section);
+
+        DataSourceConfig dsConfig = new DataSourceConfig();
+        dsConfig.setUrl(String.format("jdbc:%s://%s:%s/%s", driver.getDriverName(), host, port, database));
+        dsConfig.setUsername(username);
+        dsConfig.setPassword(password);
+        dsConfig.setDriver(driver.getDriverClass());
+
+        DatabaseConfig dbConfig = new DatabaseConfig();
+        dbConfig.setName("default");
+        dbConfig.setDataSourceConfig(dsConfig);
+
+        try {
+            return DatabaseFactory.create(dbConfig);
+        } catch (Exception exception) {
+            throw new ConfigurationException("Failed to create database connection", exception);
+        }
+    }
+
+    private DatabaseDriver loadDriver(ConfigurationSection section) throws ConfigurationException {
+
+        String driverName = section.getString("driver");
+
+        if(driverName == null) {
+            throw new ConfigurationException(String.format("Property '%s.driver' cannot be null", section.getCurrentPath()));
+        }
+
+        return Arrays.stream(DatabaseDriver.values())
+                .filter(driver -> driver.getDriverName().equalsIgnoreCase(driverName))
+                .findFirst()
+                .orElseThrow(() -> new ConfigurationException(String.format("Property '%s' at '%s.driver' is invalid", driverName, section.getCurrentPath())));
+    }
---
+++ src/main/java/com/github/syr0ws/minewaypoints/exception/ConfigurationException.java
@@ -0,0 +1,12 @@
+package com.github.syr0ws.minewaypoints.exception;
+
+public class ConfigurationException extends Exception {
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
