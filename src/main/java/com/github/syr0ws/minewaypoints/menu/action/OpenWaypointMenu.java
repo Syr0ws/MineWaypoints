04d65f6c31be2cbe45dffb9fec81026e5695ee96
Syrows <clement.gho@gmail.com>
2024-12-31 22:04:08 +0100

    fixing waypoint retrieval

--- src/main/java/com/github/syr0ws/minewaypoints/menu/action/OpenWaypointMenu.java
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/action/OpenWaypointMenu.java
@@ -5,6 +5,7 @@ import com.github.syr0ws.craftventory.api.inventory.CraftVentory;
 import com.github.syr0ws.craftventory.api.inventory.InventoryViewManager;
 import com.github.syr0ws.craftventory.api.inventory.InventoryViewer;
 import com.github.syr0ws.craftventory.api.inventory.action.ClickType;
+import com.github.syr0ws.craftventory.api.inventory.data.DataStore;
 import com.github.syr0ws.craftventory.api.inventory.event.CraftVentoryClickEvent;
 import com.github.syr0ws.craftventory.api.inventory.exception.InventoryException;
 import com.github.syr0ws.craftventory.api.transform.InventoryProvider;
@@ -33,11 +34,9 @@ public abstract class OpenWaypointMenu extends CommonAction {
         InventoryViewer viewer = event.getViewer();
         InventoryViewManager viewManager = viewer.getViewManager();
 
-        Optional<Waypoint> optional = event.getItem()
-                .filter(item -> item.getLocalStore().hasData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class))
-                .flatMap(item -> item.getLocalStore().getData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class));
+        Optional<Waypoint> optional = this.getWaypoint(event);
 
-        // The item is not paginated.
+        // Waypoint not found.
         if (optional.isEmpty()) {
             return;
         }
@@ -55,5 +54,24 @@ public abstract class OpenWaypointMenu extends CommonAction {
 
         CraftVentory inventory = provider.createInventory(service, viewer.getPlayer());
         viewManager.openView(inventory, false, context);
+    }
+
+    private Optional<Waypoint> getWaypoint(CraftVentoryClickEvent event) {
+
+        // Trying to get the waypoint from the item local store if the clicked item is paginated.
+        Optional<Waypoint> optional = event.getItem()
+                .filter(item -> item.getLocalStore().hasData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class))
+                .flatMap(item -> item.getLocalStore().getData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class));
+
+        // The item is not paginated.
+        if (optional.isPresent()) {
+            return optional;
+        }
+
+        // Trying to get the waypoint from the inventory local store.
+        CraftVentory inventory = event.getInventory();
+        DataStore store = inventory.getLocalStore();
+
+        return store.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class);
     }
 }
