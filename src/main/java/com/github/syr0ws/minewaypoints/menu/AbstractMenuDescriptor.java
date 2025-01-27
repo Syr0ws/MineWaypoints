f6c143a847cba7ad52f910c90ea0b83122494cc4
Syrows <clement.gho@gmail.com>
2025-01-27 21:43:06 +0100

    adding reusable classes to factorize code

---
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/AbstractMenuDescriptor.java
@@ -0,0 +1,34 @@
+package com.github.syr0ws.minewaypoints.menu;
+
+import com.github.syr0ws.craftventory.api.config.dao.InventoryConfigDAO;
+import com.github.syr0ws.craftventory.api.transform.InventoryDescriptor;
+import org.bukkit.plugin.Plugin;
+
+public abstract class AbstractMenuDescriptor implements InventoryDescriptor {
+
+    private final Plugin plugin;
+    private final InventoryConfigDAO inventoryConfigDAO;
+
+    public AbstractMenuDescriptor(Plugin plugin, InventoryConfigDAO inventoryConfigDAO) {
+
+        if(plugin == null) {
+            throw new IllegalArgumentException("plugin cannot be null");
+        }
+
+        if(inventoryConfigDAO == null) {
+            throw new IllegalArgumentException("inventoryConfigDAO cannot be null");
+        }
+
+        this.plugin = plugin;
+        this.inventoryConfigDAO = inventoryConfigDAO;
+    }
+
+    public Plugin getPlugin() {
+        return this.plugin;
+    }
+
+    @Override
+    public InventoryConfigDAO getInventoryConfigDAO() {
+        return this.inventoryConfigDAO;
+    }
+}
---
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/util/PlaceholderUtil.java
@@ -0,0 +1,41 @@
+package com.github.syr0ws.minewaypoints.menu.util;
+
+import com.github.syr0ws.craftventory.api.transform.placeholder.PlaceholderManager;
+import com.github.syr0ws.minewaypoints.menu.placeholder.WaypointPlaceholderEnum;
+import com.github.syr0ws.minewaypoints.menu.placeholder.WaypointSharePlaceholderEnum;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Arrays;
+
+public class PlaceholderUtil {
+
+    public static void addWaypointPlaceholders(PlaceholderManager manager, Plugin plugin) {
+
+        if(manager == null) {
+            throw new IllegalArgumentException("manager cannot be null");
+        }
+
+        if(plugin == null) {
+            throw new IllegalArgumentException("plugin cannot be null");
+        }
+
+        Arrays.stream(WaypointPlaceholderEnum.values())
+                .map(placeholder -> placeholder.get(plugin))
+                .forEach(manager::addPlaceholder);
+    }
+
+    public static void addWaypointSharePlaceholders(PlaceholderManager manager, Plugin plugin) {
+
+        if(manager == null) {
+            throw new IllegalArgumentException("manager cannot be null");
+        }
+
+        if(plugin == null) {
+            throw new IllegalArgumentException("plugin cannot be null");
+        }
+
+        Arrays.stream(WaypointSharePlaceholderEnum.values())
+                .map(placeholder -> placeholder.get(plugin))
+                .forEach(manager::addPlaceholder);
+    }
+}
