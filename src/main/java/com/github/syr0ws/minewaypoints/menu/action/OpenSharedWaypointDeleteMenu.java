71918442cb7a3d2ebb2ba2bbc0cf18a7d1c0bb81
Syr0ws <clement.gho@gmail.com>
2025-01-28 19:48:17 +0100

    refactoring code

--- src/main/java/com/github/syr0ws/minewaypoints/menu/action/OpenSharedWaypointDeleteMenu.java
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/action/OpenSharedWaypointDeleteMenu.java
@@ -2,27 +2,15 @@ package com.github.syr0ws.minewaypoints.menu.action;
 
 import com.github.syr0ws.craftventory.api.inventory.action.ClickType;
 import com.github.syr0ws.minewaypoints.menu.SharedWaypointDeleteMenuDescriptor;
-import com.github.syr0ws.minewaypoints.menu.data.CustomDataStoreKey;
-import com.github.syr0ws.minewaypoints.model.WaypointShare;
 
 import java.util.Set;
 
-public class OpenSharedWaypointDeleteMenu extends OpenContextualMenu<WaypointShare> {
+public class OpenSharedWaypointDeleteMenu extends OpenWaypointShareMenu {
 
     public static final String ACTION_NAME = "OPEN_SHARED_WAYPOINT_DELETE_MENU";
 
     public OpenSharedWaypointDeleteMenu(Set<ClickType> clickTypes) {
         super(clickTypes);
-    }
-
-    @Override
-    protected String getDataKey() {
-        return CustomDataStoreKey.WAYPOINT_SHARE;
-    }
-
-    @Override
-    protected Class<WaypointShare> getDataType() {
-        return WaypointShare.class;
     }
 
     @Override
