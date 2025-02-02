5685cf29e7bf6bf19e63ad1d1ff331016c7d4fb0
Syrows <clement.gho@gmail.com>
2025-02-02 19:42:55 +0100

    using hikaricp for sqlite

--- src/main/java/com/github/syr0ws/minewaypoints/database/connection/impl/CommonDatabaseConnection.java
+++ src/main/java/com/github/syr0ws/minewaypoints/database/connection/impl/CommonDatabaseConnection.java
@@ -1,85 +1,22 @@
 package com.github.syr0ws.minewaypoints.database.connection.impl;
 
-import com.github.syr0ws.minewaypoints.database.connection.DatabaseConnectionConfig;
 import com.github.syr0ws.minewaypoints.database.DatabaseDriver;
-import com.github.syr0ws.minewaypoints.database.connection.DatabaseConnection;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import com.github.syr0ws.minewaypoints.database.connection.DatabaseConnectionConfig;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public class CommonDatabaseConnection implements DatabaseConnection {
-
-    private final DatabaseConnectionConfig config;
-    private HikariDataSource dataSource;
+public class CommonDatabaseConnection extends HikariDatabaseConnection {
 
     public CommonDatabaseConnection(DatabaseConnectionConfig config) {
-
-        if(config == null) {
-            throw new IllegalArgumentException("config cannot be null");
-        }
-
-        this.config = config;
+        super(config);
     }
 
     @Override
-    public void open() {
-
-        if(!this.isClosed()) {
-            throw new IllegalStateException("Connection already open");
-        }
-
-        DatabaseDriver driver = this.config.getDriver();
-
-        String url = String.format("jdbc:%s://%s:%d/%s?tcpKeepAlive=true",
+    protected String getUrl(DatabaseConnectionConfig config) {
+        DatabaseDriver driver = config.getDriver();
+        return String.format("jdbc:%s://%s:%d/%s?tcpKeepAlive=true",
                 driver.getDriverName(),
-                this.config.getHost(),
-                this.config.getPort(),
-                this.config.getDatabase()
+                config.getHost(),
+                config.getPort(),
+                config.getDatabase()
         );
-
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(url);
-        hikariConfig.setDriverClassName(driver.getDriverClass());
-        hikariConfig.setUsername(this.config.getUsername());
-        hikariConfig.setPassword(this.config.getPassword());
-        hikariConfig.setAutoCommit(true);
-
-        this.dataSource = new HikariDataSource(hikariConfig);
-    }
-
-    @Override
-    public void close() throws SQLException {
-
-        if(this.isClosed()) {
-            throw new SQLException("Connection already closed");
-        }
-
-        this.dataSource.close();
-    }
-
-    @Override
-    public boolean isClosed() {
-        return this.dataSource == null || this.dataSource.isClosed();
-    }
-
-    @Override
-    public Connection getConnection() {
-
-        if(this.isClosed()) {
-            throw new IllegalStateException("Connection closed");
-        }
-
-        try {
-            return this.dataSource.getConnection();
-        } catch (SQLException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
-
-    @Override
-    public DatabaseDriver getDriver() {
-        return this.config.getDriver();
     }
 }
--- src/main/java/com/github/syr0ws/minewaypoints/database/connection/impl/SQLiteDatabaseConnection.java
+++ src/main/java/com/github/syr0ws/minewaypoints/database/connection/impl/SQLiteDatabaseConnection.java
@@ -1,81 +1,27 @@
 package com.github.syr0ws.minewaypoints.database.connection.impl;
 
 import com.github.syr0ws.minewaypoints.database.connection.DatabaseConnectionConfig;
-import com.github.syr0ws.minewaypoints.database.DatabaseDriver;
-import com.github.syr0ws.minewaypoints.database.connection.DatabaseConnection;
 import org.bukkit.plugin.Plugin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class SQLiteDatabaseConnection implements DatabaseConnection {
+public class SQLiteDatabaseConnection extends HikariDatabaseConnection {
 
     private static final String DATABASE_FILE = "database.db";
 
     private final Plugin plugin;
-    private final DatabaseConnectionConfig config;
-    private Connection connection;
 
     public SQLiteDatabaseConnection(Plugin plugin, DatabaseConnectionConfig config) {
+        super(config);
 
         if(plugin == null) {
             throw new IllegalArgumentException("plugin cannot be null");
         }
 
-        if(config == null) {
-            throw new IllegalArgumentException("config cannot be null");
-        }
-
         this.plugin = plugin;
-        this.config = config;
     }
 
     @Override
-    public void open() throws SQLException {
-
-        if(!this.isClosed()) {
-            throw new IllegalStateException("Connection already open");
-        }
-
+    protected String getUrl(DatabaseConnectionConfig config) {
         String path = this.plugin.getDataFolder().getAbsolutePath() + "/" + DATABASE_FILE;
-        String url = "jdbc:sqlite:" + path;
-
-        this.connection = DriverManager.getConnection(url);
-        this.connection.setAutoCommit(true);
-    }
-
-    @Override
-    public void close() throws SQLException {
-
-        if(this.isClosed()) {
-            throw new IllegalStateException("Connection already closed");
-        }
-
-        this.connection.close();
-    }
-
-    @Override
-    public boolean isClosed() {
-        try {
-            return this.connection == null || this.connection.isClosed();
-        } catch (SQLException exception) {
-            return false;
-        }
-    }
-
-    @Override
-    public Connection getConnection() {
-
-        if(this.isClosed()) {
-            throw new IllegalStateException("Connection closed");
-        }
-
-        return this.connection;
-    }
-
-    @Override
-    public DatabaseDriver getDriver() {
-        return this.config.getDriver();
+        return "jdbc:sqlite:" + path;
     }
 }
