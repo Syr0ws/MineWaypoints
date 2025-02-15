e62b86657bac9658a2503960eba1fd9257cc68f8
Syrows <clement.gho@gmail.com>
2025-02-15 22:58:43 +0100

    fixing waypoint retrieval

--- src/main/java/com/github/syr0ws/minewaypoints/menu/action/ToggleWaypointActivation.java
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/action/ToggleWaypointActivation.java
@@ -1,12 +1,14 @@
 package com.github.syr0ws.minewaypoints.menu.action;
 
 import com.github.syr0ws.crafter.util.Validate;
+import com.github.syr0ws.craftventory.api.inventory.CraftVentory;
 import com.github.syr0ws.craftventory.api.inventory.InventoryViewer;
 import com.github.syr0ws.craftventory.api.inventory.action.ClickType;
 import com.github.syr0ws.craftventory.api.inventory.data.DataStore;
 import com.github.syr0ws.craftventory.api.inventory.event.CraftVentoryClickEvent;
 import com.github.syr0ws.craftventory.api.inventory.item.InventoryItem;
 import com.github.syr0ws.craftventory.common.inventory.action.CommonAction;
+import com.github.syr0ws.craftventory.common.inventory.data.CommonDataStoreKey;
 import com.github.syr0ws.minewaypoints.menu.data.CustomDataStoreKey;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
 import com.github.syr0ws.minewaypoints.service.WaypointActivationService;
@@ -36,21 +38,15 @@ public class ToggleWaypointActivation extends CommonAction {
     public void execute(CraftVentoryClickEvent event) {
 
         InventoryViewer viewer = event.getViewer();
+        Waypoint waypoint = this.getWaypoint(event);
 
-        InventoryItem item = event.getItem()
-                .orElseThrow(() -> new NullPointerException("No item clicked"));
-
-        DataStore store = item.getLocalStore();
-
-        // Only items with a waypoint in their data store (typically paginated waypoint items)
-        // can execute this action.
-        Waypoint waypoint = store.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class)
-                .orElseThrow(() -> new IllegalStateException("No waypoint found in item local store"));
+        InventoryItem item = event.getItem().get();
 
         // Activating the waypoint.
         item.disable();
 
-        // TODO Catch errors.
+        // TODO Catch errors
+        // TODO Update view
         this.waypointActivationService.activateWaypoint(viewer.getPlayer(), waypoint.getId())
                 .complete(item::enable)
                 .resolveAsync(this.plugin);
@@ -59,5 +55,33 @@ public class ToggleWaypointActivation extends CommonAction {
     @Override
     public String getName() {
         return ACTION_NAME;
+    }
+
+    private Waypoint getWaypoint(CraftVentoryClickEvent event) {
+
+        InventoryItem item = event.getItem()
+                .orElseThrow(() -> new NullPointerException("No item clicked"));
+
+        DataStore itemStore = item.getLocalStore();
+
+        // Case 1: Waypoint is stored directly in the item local store.
+        if(itemStore.hasData(CustomDataStoreKey.WAYPOINT, Waypoint.class)) {
+            return itemStore.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class).get();
+        }
+
+        // Case 2: Waypoint is stored as a paginated data.
+        if(itemStore.hasData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class)) {
+            return itemStore.getData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class).get();
+        }
+
+        // Case 3: Waypoint is stored in the inventory local store.
+        CraftVentory inventory = event.getInventory();
+        DataStore inventoryStore = inventory.getLocalStore();
+
+        if(inventoryStore.hasData(CustomDataStoreKey.WAYPOINT, Waypoint.class)) {
+            return inventoryStore.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class).get();
+        }
+
+        throw new IllegalStateException("No waypoint found");
     }
 }
