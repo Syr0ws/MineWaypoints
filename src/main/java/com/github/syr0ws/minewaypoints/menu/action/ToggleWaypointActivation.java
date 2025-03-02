e5239471d3afe3efd8481b96728201c5eb643c5e
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2025-03-02 11:00:52 +0100

    fix: shared waypoint activation (#42)

--- src/main/java/com/github/syr0ws/minewaypoints/menu/action/ToggleWaypointActivation.java
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/action/ToggleWaypointActivation.java
@@ -10,11 +10,14 @@ import com.github.syr0ws.craftventory.api.inventory.item.InventoryItem;
 import com.github.syr0ws.craftventory.common.inventory.action.CommonAction;
 import com.github.syr0ws.craftventory.common.inventory.data.CommonDataStoreKey;
 import com.github.syr0ws.minewaypoints.menu.data.CustomDataStoreKey;
+import com.github.syr0ws.minewaypoints.menu.util.DataUtil;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
+import com.github.syr0ws.minewaypoints.model.WaypointShare;
 import com.github.syr0ws.minewaypoints.service.WaypointActivationService;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -66,27 +69,16 @@ public class ToggleWaypointActivation extends CommonAction {
 
     private Waypoint getWaypoint(CraftVentoryClickEvent event) {
 
-        InventoryItem item = event.getItem()
-                .orElseThrow(() -> new NullPointerException("No item clicked"));
+        Optional<Waypoint> waypointOptional = DataUtil.getContextualData(event, CustomDataStoreKey.WAYPOINT, Waypoint.class);
 
-        DataStore itemStore = item.getLocalStore();
-
-        // Case 1: Waypoint is stored directly in the item local store.
-        if (itemStore.hasData(CustomDataStoreKey.WAYPOINT, Waypoint.class)) {
-            return itemStore.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class).get();
+        if(waypointOptional.isPresent()) {
+            return waypointOptional.get();
         }
 
-        // Case 2: Waypoint is stored as a paginated data.
-        if (itemStore.hasData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class)) {
-            return itemStore.getData(CommonDataStoreKey.PAGINATED_DATA, Waypoint.class).get();
-        }
+        Optional<WaypointShare> waypointShareOptional = DataUtil.getContextualData(event, CustomDataStoreKey.WAYPOINT_SHARE, WaypointShare.class);
 
-        // Case 3: Waypoint is stored in the inventory local store.
-        CraftVentory inventory = event.getInventory();
-        DataStore inventoryStore = inventory.getLocalStore();
-
-        if (inventoryStore.hasData(CustomDataStoreKey.WAYPOINT, Waypoint.class)) {
-            return inventoryStore.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class).get();
+        if(waypointShareOptional.isPresent()) {
+            return waypointShareOptional.get().getWaypoint();
         }
 
         throw new IllegalStateException("No waypoint found");
