ad47f60494ac6b16cee62739cecaec0c0c3e3139
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2025-03-08 18:11:50 +0100

    rework: events (#60)

--- src/main/java/com/github/syr0ws/minewaypoints/event/WaypointDeleteEvent.java
+++ src/main/java/com/github/syr0ws/minewaypoints/event/WaypointDeleteEvent.java
@@ -3,11 +3,14 @@ package com.github.syr0ws.minewaypoints.event;
 import com.github.syr0ws.crafter.util.Validate;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
 import com.github.syr0ws.minewaypoints.model.WaypointUser;
+import org.bukkit.event.HandlerList;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class WaypointDeleteEvent extends WaypointEvent {
+
+    private static final HandlerList handlers = new HandlerList();
 
     private final Set<WaypointUser> waypointSharedWith;
 
@@ -20,5 +23,14 @@ public class WaypointDeleteEvent extends WaypointEvent {
 
     public Set<WaypointUser> getWaypointSharedWith() {
         return this.waypointSharedWith;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
--- src/main/java/com/github/syr0ws/minewaypoints/event/WaypointUnshareEvent.java
+++ src/main/java/com/github/syr0ws/minewaypoints/event/WaypointUnshareEvent.java
@@ -3,8 +3,11 @@ package com.github.syr0ws.minewaypoints.event;
 import com.github.syr0ws.crafter.util.Validate;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
 import com.github.syr0ws.minewaypoints.model.WaypointUser;
+import org.bukkit.event.HandlerList;
 
 public class WaypointUnshareEvent extends WaypointEvent {
+
+    private static final HandlerList handlers = new HandlerList();
 
     private final WaypointUser sharedWith;
 
@@ -17,5 +20,14 @@ public class WaypointUnshareEvent extends WaypointEvent {
 
     public WaypointUser getSharedWith() {
         return this.sharedWith;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
