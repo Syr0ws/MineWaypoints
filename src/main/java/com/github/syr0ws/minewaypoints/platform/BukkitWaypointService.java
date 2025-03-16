01a6cc8c77c0dbd261d795d2ddad0e34a202845b
Syrows <clement.gho@gmail.com>
2025-03-16 09:36:51 +0100

    returning business results instead of business objects

--- src/main/java/com/github/syr0ws/minewaypoints/platform/BukkitWaypointService.java
+++ src/main/java/com/github/syr0ws/minewaypoints/platform/BukkitWaypointService.java
@@ -1,5 +1,6 @@
 package com.github.syr0ws.minewaypoints.platform;
 
+import com.github.syr0ws.crafter.business.BusinessResult;
 import com.github.syr0ws.crafter.util.Promise;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
 import com.github.syr0ws.minewaypoints.model.WaypointShare;
@@ -11,17 +12,17 @@ import java.util.List;
 
 public interface BukkitWaypointService {
 
-    Promise<Waypoint> createWaypoint(Player owner, String name, Material icon, Location location);
+    Promise<BusinessResult<Waypoint, ?>> createWaypoint(Player owner, String name, Material icon, Location location);
 
-    Promise<Waypoint> updateWaypointNameByName(Player owner, String waypointName, String newName);
+    Promise<BusinessResult<Waypoint, ?>> updateWaypointNameByName(Player owner, String waypointName, String newName);
 
-    Promise<Waypoint> updateWaypointIconById(Player owner, long waypointId, Material icon);
+    Promise<BusinessResult<Waypoint, ?>> updateWaypointIconById(Player owner, long waypointId, Material icon);
 
-    Promise<Void> deleteWaypoint(Player owner, long waypointId);
+    Promise<BusinessResult<Void, ?>> deleteWaypoint(Player owner, long waypointId);
 
-    Promise<WaypointShare> shareWaypoint(Player owner, long waypointId, String targetName);
+    Promise<BusinessResult<WaypointShare, ?>> shareWaypoint(Player owner, long waypointId, String targetName);
 
-    Promise<Void> unshareWaypointByOwner(Player owner, long waypointId, String targetName);
+    Promise<BusinessResult<Void, ?>> unshareWaypointByOwner(Player owner, long waypointId, String targetName);
 
     Promise<List<WaypointShare>> getSharedWaypoints(Player player);
 
--- src/main/java/com/github/syr0ws/minewaypoints/platform/impl/SimpleBukkitWaypointService.java
+++ src/main/java/com/github/syr0ws/minewaypoints/platform/impl/SimpleBukkitWaypointService.java
@@ -1,8 +1,8 @@
 package com.github.syr0ws.minewaypoints.platform.impl;
 
+import com.github.syr0ws.crafter.business.BusinessResult;
 import com.github.syr0ws.crafter.config.ConfigUtil;
 import com.github.syr0ws.crafter.config.ConfigurationException;
-import com.github.syr0ws.crafter.message.MessageUtil;
 import com.github.syr0ws.crafter.util.Promise;
 import com.github.syr0ws.crafter.util.Validate;
 import com.github.syr0ws.minewaypoints.business.failure.processor.WaypointFailureProcessor;
@@ -12,13 +12,12 @@ import com.github.syr0ws.minewaypoints.model.WaypointShare;
 import com.github.syr0ws.minewaypoints.platform.BukkitWaypointService;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.logging.Level;
 
 public class SimpleBukkitWaypointService implements BukkitWaypointService {
 
@@ -34,107 +33,98 @@ public class SimpleBukkitWaypointService implements BukkitWaypointService {
     }
 
     @Override
-    public Promise<Waypoint> createWaypoint(Player owner, String name, Material icon, Location location) {
+    public Promise<BusinessResult<Waypoint, ?>> createWaypoint(Player owner, String name, Material icon, Location location) {
         Validate.notNull(owner, "owner cannot be null");
         Validate.notEmpty(name, "name cannot be null or empty");
         Validate.notNull(location, "location cannot be null");
 
         UUID ownerId = owner.getUniqueId();
 
-        return new Promise<Waypoint>((resolve, reject) -> {
+        return new Promise<>((resolve, reject) -> {
 
             Material waypointIcon = icon == null ? this.getDefaultWaypointIcon() : icon;
 
-            this.waypointService.createWaypoint(ownerId, name, waypointIcon.name(), location)
-                    .onSuccess(resolve)
+            BusinessResult<Waypoint, ?> result = this.waypointService.createWaypoint(ownerId, name, waypointIcon.name(), location)
                     .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
 
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while creating the waypoint", throwable);
-            this.sendErrorMessage(owner);
+            resolve.accept(result);
         });
     }
 
     @Override
-    public Promise<Waypoint> updateWaypointNameByName(Player owner, String waypointName, String newName) {
+    public Promise<BusinessResult<Waypoint, ?>> updateWaypointNameByName(Player owner, String waypointName, String newName) {
         Validate.notNull(owner, "owner cannot be null");
         Validate.notEmpty(waypointName, "waypointName cannot be null or empty");
         Validate.notEmpty(newName, "newName cannot be null or empty");
 
         UUID ownerId = owner.getUniqueId();
 
-        return new Promise<Waypoint>((resolve, reject) -> {
-            this.waypointService.updateWaypointNameByName(ownerId, waypointName, newName)
-                    .onSuccess(resolve)
+        return new Promise<>((resolve, reject) -> {
+
+            BusinessResult<Waypoint, ?> result = this.waypointService.updateWaypointNameByName(ownerId, waypointName, newName)
                     .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while renaming the waypoint", throwable);
-            this.sendErrorMessage(owner);
+
+            resolve.accept(result);
         });
     }
 
     @Override
-    public Promise<Waypoint> updateWaypointIconById(Player owner, long waypointId, Material icon) {
+    public Promise<BusinessResult<Waypoint, ?>> updateWaypointIconById(Player owner, long waypointId, Material icon) {
         Validate.notNull(owner, "owner cannot be null");
 
         UUID ownerId = owner.getUniqueId();
 
-        return new Promise<Waypoint>((resolve, reject) -> {
+        return new Promise<>((resolve, reject) -> {
 
             Material waypointIcon = icon == null ? this.getDefaultWaypointIcon() : icon;
 
-            this.waypointService.updateWaypointIconById(ownerId, waypointId, waypointIcon.name())
-                    .onSuccess(resolve)
+            BusinessResult<Waypoint, ?> result = this.waypointService.updateWaypointIconById(ownerId, waypointId, waypointIcon.name())
                     .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while updating the waypoint icon", throwable);
-            this.sendErrorMessage(owner);
+
+            resolve.accept(result);
         });
     }
 
     @Override
-    public Promise<Void> deleteWaypoint(Player owner, long waypointId) {
+    public Promise<BusinessResult<Void, ?>> deleteWaypoint(Player owner, long waypointId) {
         Validate.notNull(owner, "owner cannot be null");
 
         UUID ownerId = owner.getUniqueId();
 
-        return new Promise<Void>((resolve, reject) -> {
-            this.waypointService.deleteWaypoint(ownerId, waypointId)
-                    .onSuccess(resolve)
+        return new Promise<>((resolve, reject) -> {
+
+            BusinessResult<Void, ?> result = this.waypointService.deleteWaypoint(ownerId, waypointId)
                     .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while updating the waypoint icon", throwable);
-            this.sendErrorMessage(owner);
+
+            resolve.accept(result);
         });
     }
 
     @Override
-    public Promise<WaypointShare> shareWaypoint(Player owner, long waypointId, String targetName) {
+    public Promise<BusinessResult<WaypointShare, ?>> shareWaypoint(Player owner, long waypointId, String targetName) {
         Validate.notNull(owner, "owner cannot be null");
         Validate.notEmpty(targetName, "targetName cannot be null or empty");
 
-        return new Promise<WaypointShare>((resolve, reject) -> {
-            this.waypointService.shareWaypoint(owner.getUniqueId(), waypointId, targetName)
-                    .onSuccess(resolve)
+        return new Promise<>((resolve, reject) -> {
+
+            BusinessResult<WaypointShare, ?> result = this.waypointService.shareWaypoint(owner.getUniqueId(), waypointId, targetName)
                     .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while sharing the waypoint", throwable);
-            this.sendErrorMessage(owner);
+
+            resolve.accept(result);
         });
     }
 
     @Override
-    public Promise<Void> unshareWaypointByOwner(Player owner, long waypointId, String targetName) {
+    public Promise<BusinessResult<Void, ?>> unshareWaypointByOwner(Player owner, long waypointId, String targetName) {
         Validate.notNull(owner, "owner cannot be null");
         Validate.notEmpty(targetName, "targetName cannot be null or empty");
 
-        return new Promise<Void>((resolve, reject) -> {
-            this.waypointService.unshareWaypointByOwner(owner.getUniqueId(), waypointId, targetName)
-                    .onSuccess(resolve)
+        return new Promise<>((resolve, reject) -> {
+
+            BusinessResult<Void, ?> result = this.waypointService.unshareWaypointByOwner(owner.getUniqueId(), waypointId, targetName)
                     .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while unsharing the waypoint", throwable);
-            this.sendErrorMessage(owner);
+
+            resolve.accept(result);
         });
     }
 
@@ -142,13 +132,10 @@ public class SimpleBukkitWaypointService implements BukkitWaypointService {
     public Promise<List<WaypointShare>> getSharedWaypoints(Player player) {
         Validate.notNull(player, "player cannot be null");
 
-        return new Promise<List<WaypointShare>>((resolve, reject) -> {
+        return new Promise<>((resolve, reject) -> {
             this.waypointService.getSharedWaypoints(player.getUniqueId())
                     .onSuccess(resolve)
-                    .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, player).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while retrieving user' shared waypoints", throwable);
-            this.sendErrorMessage(player);
+                    .onFailure(ignored -> resolve.accept(new ArrayList<>()));
         });
     }
 
@@ -156,19 +143,11 @@ public class SimpleBukkitWaypointService implements BukkitWaypointService {
     public Promise<List<WaypointShare>> getSharedWith(Player owner, long waypointId) {
         Validate.notNull(owner, "owner cannot be null");
 
-        return new Promise<List<WaypointShare>>((resolve, reject) -> {
+        return new Promise<>((resolve, reject) -> {
             this.waypointService.getSharedWith(waypointId)
                     .onSuccess(resolve)
-                    .onFailure(failure -> WaypointFailureProcessor.of(this.plugin, owner).process(failure));
-        }).except(throwable -> {
-            this.plugin.getLogger().log(Level.SEVERE, "An error occurred while retrieving the users the waypoint is shared with", throwable);
-            this.sendErrorMessage(owner);
+                    .onFailure(ignored -> resolve.accept(new ArrayList<>()));
         });
-    }
-
-    private void sendErrorMessage(Player player) {
-        FileConfiguration config = this.plugin.getConfig();
-        MessageUtil.sendMessage(player, config, "errors.generic");
     }
 
     private Material getDefaultWaypointIcon() {
