82c39d1c428494eba8896fea5227efb26e5a35fa
Syrows <clement.gho@gmail.com>
2025-03-20 22:02:14 +0100

    fixing class

--- src/main/java/com/github/syr0ws/minewaypoints/menu/WaypointSharedWithMenuDescriptor.java
+++ src/main/java/com/github/syr0ws/minewaypoints/menu/WaypointSharedWithMenuDescriptor.java
@@ -13,7 +13,8 @@ import com.github.syr0ws.minewaypoints.menu.hook.WaypointInitStoreHook;
 import com.github.syr0ws.minewaypoints.menu.util.PlaceholderUtil;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
 import com.github.syr0ws.minewaypoints.model.WaypointShare;
-import com.github.syr0ws.minewaypoints.service.WaypointService;
+import com.github.syr0ws.minewaypoints.platform.BukkitWaypointService;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.nio.file.Path;
@@ -21,16 +22,15 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.logging.Level;
 
 public class WaypointSharedWithMenuDescriptor extends AbstractMenuDescriptor {
 
     public static final String MENU_ID = "waypoint-shared-with-menu";
     private static final String MENU_CONFIG_PATH = "menus/waypoint-shared-with-menu.yml";
 
-    private final WaypointService waypointService;
+    private final BukkitWaypointService waypointService;
 
-    public WaypointSharedWithMenuDescriptor(Plugin plugin, InventoryConfigDAO inventoryConfigDAO, WaypointService waypointService) {
+    public WaypointSharedWithMenuDescriptor(Plugin plugin, InventoryConfigDAO inventoryConfigDAO, BukkitWaypointService waypointService) {
         super(plugin, inventoryConfigDAO);
         Validate.notNull(waypointService, "waypointService cannot be null");
 
@@ -42,6 +42,7 @@ public class WaypointSharedWithMenuDescriptor extends AbstractMenuDescriptor {
 
         manager.addProvider(new PaginationProvider<>("waypoint-shared-with-pagination", WaypointShare.class, (inventory, pagination) -> {
 
+            Player player = inventory.getViewer().getPlayer();
             DataStore store = inventory.getLocalStore();
 
             Waypoint waypoint = store.getData(CustomDataStoreKey.WAYPOINT, Waypoint.class)
@@ -49,18 +50,13 @@ public class WaypointSharedWithMenuDescriptor extends AbstractMenuDescriptor {
 
             List<WaypointShare> list = new ArrayList<>();
 
-            this.waypointService.getSharedWith(waypoint.getId())
+            this.waypointService.getSharedWith(player, waypoint.getId())
                     .then(waypointShares -> {
                         waypointShares.sort(Comparator.comparing(share -> share.getSharedWith().getName()));
 
                         pagination.getModel().updateItems(waypointShares);
                         pagination.update(false);
-                    })
-                    .except(throwable -> {
-                        String message = String.format("An error occurred while retrieving players the waypoint %d is shared with", waypoint.getId());
-                        super.getPlugin().getLogger().log(Level.SEVERE, message, throwable);
-                    })
-                    .resolveAsync(super.getPlugin());
+                    }).resolveAsync(super.getPlugin());
 
             return list;
         }));
