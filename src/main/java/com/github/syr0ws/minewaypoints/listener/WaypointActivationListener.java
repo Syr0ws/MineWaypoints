39f59910fbe013330c6c259a933dfbd7322b8799
Syrows <clement.gho@gmail.com>
2025-03-22 11:22:46 +0100

    removing messages

--- src/main/java/com/github/syr0ws/minewaypoints/listener/WaypointActivationListener.java
+++ src/main/java/com/github/syr0ws/minewaypoints/listener/WaypointActivationListener.java
@@ -1,7 +1,5 @@
 package com.github.syr0ws.minewaypoints.listener;
 
-import com.github.syr0ws.crafter.message.MessageUtil;
-import com.github.syr0ws.crafter.message.placeholder.Placeholder;
 import com.github.syr0ws.crafter.util.Validate;
 import com.github.syr0ws.minewaypoints.cache.WaypointVisibleCache;
 import com.github.syr0ws.minewaypoints.event.WaypointDeleteEvent;
@@ -9,10 +7,8 @@ import com.github.syr0ws.minewaypoints.event.WaypointUnshareEvent;
 import com.github.syr0ws.minewaypoints.model.Waypoint;
 import com.github.syr0ws.minewaypoints.model.WaypointUser;
 import com.github.syr0ws.minewaypoints.platform.BukkitWaypointActivationService;
-import com.github.syr0ws.minewaypoints.util.placeholder.PlaceholderUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,8 +19,6 @@ import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -107,15 +101,6 @@ public class WaypointActivationListener implements Listener {
         // The deleted waypoint should no longer be visible to players it has been shared with.
         // Note: As the waypoint is deleted, there is no need to deactivate it as data will be automatically cleared.
         this.waypointVisibleCache.hideWaypoint(waypoint);
-
-        // Sending a message to online players with the deleted waypoint shared to indicate that it has been deleted.
-        FileConfiguration config = this.plugin.getConfig();
-        Map<Placeholder, String> placeholders = PlaceholderUtil.getWaypointPlaceholders(this.plugin, waypoint);
-
-        waypointSharedWith.stream()
-                .map(WaypointUser::getPlayer)
-                .filter(Objects::nonNull) // Case in which the player is online.
-                .forEach(player -> MessageUtil.sendMessage(player, config, "messages.waypoint.shared-deleted", placeholders));
     }
 
     @EventHandler
@@ -125,23 +110,13 @@ public class WaypointActivationListener implements Listener {
         WaypointUser sharedWith = event.getSharedWith();
         Player playerSharedWith = sharedWith.getPlayer();
 
-        // Case in which the player is online.
+        // Hiding the waypoint if it is visible to the player it has been shared with.
         if (playerSharedWith != null) {
-
-            // Hiding the waypoint if it is visible to the player it has been shared with.
-            this.waypointVisibleCache.hideWaypoint(playerSharedWith);
-
-            // Sending a message to the user the waypoint is shared with if it is online to indicate
-            // that the waypoint has been unshared.
-            FileConfiguration config = this.plugin.getConfig();
-            Map<Placeholder, String> placeholders = PlaceholderUtil.getWaypointPlaceholders(this.plugin, waypoint);
-
-            MessageUtil.sendMessage(playerSharedWith, config, "messages.waypoint.unshare-target", placeholders);
+            this.waypointVisibleCache.hideWaypoint(playerSharedWith, waypoint);
         }
     }
 
     private void showWaypointIfAny(Player player, World world) {
-        System.out.println(world.getName());
         this.waypointActivationService.getActivatedWaypoint(player.getUniqueId(), world)
                 .then(optional ->
                         optional.ifPresent(waypoint -> {
