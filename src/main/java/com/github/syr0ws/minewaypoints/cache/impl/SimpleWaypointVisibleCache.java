5a86f8a0d9da66a48d5a44ad39c922a03c4272c6
Syrows <clement.gho@gmail.com>
2025-03-22 13:53:03 +0100

    fixing removal

--- src/main/java/com/github/syr0ws/minewaypoints/cache/impl/SimpleWaypointVisibleCache.java
+++ src/main/java/com/github/syr0ws/minewaypoints/cache/impl/SimpleWaypointVisibleCache.java
@@ -38,7 +38,8 @@ public class SimpleWaypointVisibleCache implements WaypointVisibleCache {
     public void hideWaypoint(Player player, Waypoint waypoint) {
         Validate.notNull(player, "player cannot be null");
         Validate.notNull(waypoint, "waypoint cannot be null");
-        this.visibleWaypoints.remove(player, waypoint);
+        this.visibleWaypoints.entrySet().removeIf(entry -> entry.getKey().equals(player)
+                && entry.getValue().getId() == waypoint.getId());
     }
 
     @Override
