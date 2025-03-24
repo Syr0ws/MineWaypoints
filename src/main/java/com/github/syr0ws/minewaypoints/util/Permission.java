a0ebc927777dd185d9249b47f3b8b8804ce77737
Syrows <clement.gho@gmail.com>
2025-03-24 20:14:46 +0100

    fix: remove unused permission

--- src/main/java/com/github/syr0ws/minewaypoints/util/Permission.java
+++ src/main/java/com/github/syr0ws/minewaypoints/util/Permission.java
@@ -6,8 +6,7 @@ public enum Permission {
     COMMAND_WAYPOINTS_CREATE("command.waypoints.create"),
     COMMAND_WAYPOINTS_RENAME("command.waypoints.reload"),
     COMMAND_WAYPOINTS_RELOCATE("command.waypoints.reload"),
-    COMMAND_WAYPOINTS_SHARE("command.waypoints.share"),
-    COMMAND_WAYPOINTS_UNSHARE("command.waypoints.unshare");
+    COMMAND_WAYPOINTS_SHARE("command.waypoints.share");
 
     private static final String PERMISSION_PREFIX = "minewaypoints";
 
