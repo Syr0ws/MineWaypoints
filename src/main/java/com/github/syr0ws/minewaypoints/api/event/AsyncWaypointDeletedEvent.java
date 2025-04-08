162681a4724b0fea43cf0e341e565d13a23d5ad2
Syrows <clement.gho@gmail.com>
2025-04-08 20:36:27 +0200

    improving code

---
+++ src/main/java/com/github/syr0ws/minewaypoints/api/event/AsyncWaypointDeletedEvent.java
@@ -0,0 +1,32 @@
+package com.github.syr0ws.minewaypoints.api.event;
+
+import com.github.syr0ws.crafter.util.Validate;
+import com.github.syr0ws.minewaypoints.model.Waypoint;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+public class AsyncWaypointDeletedEvent extends WaypointEvent {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player owner;
+
+    public AsyncWaypointDeletedEvent(Waypoint waypoint, Player owner) {
+        super(waypoint, true);
+        Validate.notNull(owner, "owner cannot be null");
+        this.owner = owner;
+    }
+
+    public Player getOwner() {
+        return this.owner;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
