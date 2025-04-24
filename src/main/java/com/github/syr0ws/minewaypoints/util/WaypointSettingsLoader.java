a72d4253a4c1042044e74e6b641d41d4496d06a4
Syrows <clement.gho@gmail.com>
2025-04-24 22:06:31 +0200

    renaming key

--- src/main/java/com/github/syr0ws/minewaypoints/util/WaypointSettingsLoader.java
+++ src/main/java/com/github/syr0ws/minewaypoints/util/WaypointSettingsLoader.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class WaypointSettingsLoader {
 
-    private static final String WAYPOINTS_CREATE_LIMITS_KEY = "waypoint-create-limits";
+    private static final String WAYPOINTS_CREATION_LIMITS_KEY = "waypoint-creation-limits";
 
     public WaypointSettings loadWaypointSettings(FileConfiguration config) throws ConfigurationException {
 
@@ -22,20 +22,20 @@ public class WaypointSettingsLoader {
     private List<WaypointLimitPermission> loadWaypointCreateLimitPermissions(FileConfiguration config) throws ConfigurationException {
 
         List<WaypointLimitPermission> permissions = new ArrayList<>();
-        List<Map<?,?>> mapList = config.getMapList("waypoint-create-limits");
+        List<Map<?,?>> mapList = config.getMapList(WAYPOINTS_CREATION_LIMITS_KEY);
 
         for(Map<?,?> map : mapList) {
 
             if(!map.containsKey("permission") || !(map.get("permission") instanceof String permission)) {
-                throw new ConfigurationException("Missing or invalid key 'permission' at '%s'".formatted(WAYPOINTS_CREATE_LIMITS_KEY));
+                throw new ConfigurationException("Missing or invalid key 'permission' at '%s'".formatted(WAYPOINTS_CREATION_LIMITS_KEY));
             }
 
             if(!map.containsKey("limit") || !(map.get("limit") instanceof Integer limit)) {
-                throw new ConfigurationException("Missing or invalid key 'limit' at '%s'".formatted(WAYPOINTS_CREATE_LIMITS_KEY));
+                throw new ConfigurationException("Missing or invalid key 'limit' at '%s'".formatted(WAYPOINTS_CREATION_LIMITS_KEY));
             }
 
             if(limit <= 0 && limit != -1) {
-                throw new ConfigurationException("Invalid limit %d at '%s': limit must be strictly positive or -1".formatted(limit, WAYPOINTS_CREATE_LIMITS_KEY));
+                throw new ConfigurationException("Invalid limit %d at '%s': limit must be strictly positive or -1".formatted(limit, WAYPOINTS_CREATION_LIMITS_KEY));
             }
 
             permissions.add(new WaypointLimitPermission(permission, limit == -1 ? Integer.MAX_VALUE : limit));
